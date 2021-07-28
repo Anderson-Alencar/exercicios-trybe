9dff336fb527b7e34325a22c30435ac174dc3607
Anderson Alencar <anderson23alencar@gmail.com>
2021-07-28 15:45:45 -0300

    Variáveis

---
+++ fundamentos/bloco-04-introducao-a-javascript/dia-01-js-primeiros-passos/para-fixar.js
@@ -0,0 +1,10 @@
+const myName = "Anderson";
+const birthCity = "Fortaleza-CE";
+let birthYear = 1995;
+
+birthYear = 2030;
+birthCity = "Madrid";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
