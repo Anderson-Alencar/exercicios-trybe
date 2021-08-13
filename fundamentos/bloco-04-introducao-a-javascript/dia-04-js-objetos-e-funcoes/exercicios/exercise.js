b98f449e25ec6c33f1f5c11c9e7dc7cf4325d8a4
Anderson Alencar <anderson23alencar@gmail.com>
2021-08-13 10:06:28 -0300

    manipulando dois objetos

--- fundamentos/bloco-04-introducao-a-javascript/dia-04-js-objetos-e-funcoes/exercicios/exercise.js
+++ fundamentos/bloco-04-introducao-a-javascript/dia-04-js-objetos-e-funcoes/exercicios/exercise.js
@@ -1,9 +1,9 @@
 let info = {
-  personagem: 'Margarida',
+  personagem: 'Margarida',  
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
   recorrente: 'Sim'
-};
+}; 
 
 // 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
 
@@ -23,4 +23,21 @@ for (const dados in info) {
 
 for (const dados in info) {
   console.log(info[dados]);
+}
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+let uncleInfo = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+};
+
+for (let dados in info) {
+  if (dados === 'recorrente' && info[dados] === 'Sim' && uncleInfo[dados] === 'Sim') {
+    console.log('Ambos recorrentes');
+  } else {
+    console.log(info[dados] + ' & ' + uncleInfo[dados]);
+  }
 }
