8eb3fd488d13f6f1f9f5c785c261b2cd175d000a
Anderson-Alencar <anderson23alencar@gmail.com>
2022-02-18 16:10:15 -0300

    camadas service e controller

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
@@ -1,19 +1,5 @@
 const connection = require('./connection');
 
-const getNewAuthor = ({ id, firstName, middleName, lastName}) => {
-  const fullName = [firstName, middleName, lastName]
-    .filter(Boolean)
-    .join(' ');
-
-  return {
-    id,
-    firstName,
-    middleName,
-    lastName,
-    fullName,
-  };
-};
-
 const serialize = (authorData) => ({
   id: authorData.id,
   firstName: authorData.first_name,
@@ -25,7 +11,7 @@ const getAll = async () => {
   const [authors] = await connection.execute(
     'SELECT id_authors, first_name, middle_name, last_name FROM model_example.authors',
   );
-  return authors.map(serialize).map(getNewAuthor);
+  return authors.map(serialize);
 };
 
 const findById = async (id) => {
@@ -36,34 +22,20 @@ const findById = async (id) => {
 
   if (authorData.length === 0) return null;
 
-  const { firstName, middleName, lastName } = authorData.map(serialize)[0];
-
-  return getNewAuthor({
-    id,
-    firstName,
-    middleName,
-    lastName,
-  });
+  return  serialize(authorData)[0];
 };
 
-const isValid = (firstName, middleName, lastName) => {
-  if (!firstName || typeof firstName !== 'string') return false;
-  if (!lastName || typeof lastName !== 'string') return false;
-  if (middleName && typeof middleName !== 'string') return false;
-
-  return true;
-}
-
 const create = (firstName, middleName, lastName) => {
-  connection.execute(
+  const [author] = connection.execute(
     'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
     [firstName, middleName, lastName],
   )
+  
+  return author;
 }
 
 module.exports = {
   getAll,
   findById,
-  isValid,
   create,
 };
